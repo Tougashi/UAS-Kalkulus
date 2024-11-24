a53083480832ef5953312d9a24da8ed094543551
Tougashi <94227628+Tougashi@users.noreply.github.com>
2024-11-24 22:18:38 +0700

    s

--- api/index.php
+++ api/index.php
@@ -1,9 +1,3 @@
 <?php
 
-use Symfony\Component\Dotenv\Dotenv;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-(new Dotenv())->bootEnv(__DIR__ . '/../.env');
-
 require __DIR__ . '/../public/index.php';
